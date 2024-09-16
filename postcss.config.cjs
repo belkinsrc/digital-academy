ced0b571f6fc66fc69a0023db07a7679585dca17
Nikita Belkin <belkinsrc@gmail.com>
2024-09-16 12:29:43 +0300

    delete extra config files

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const nested = require("postcss-nested");
-const postcss = require("postcss-preset-env");
-const customMedia = require("postcss-custom-media");
-
-module.exports = {
-    plugins: [nested(), postcss(), customMedia({})]
-}
