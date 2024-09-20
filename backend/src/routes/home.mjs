2afce69418699c69d655e2655631b71d0677c0bc
Nikita Belkin <belkinsrc@gmail.com>
2024-09-20 14:10:56 +0300

    delete home.mjs

--- backend/src/routes/home.mjs
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getProducts } from '../controllers/products.mjs';
-
-const router = express.Router();
-
-router.get('/', getProducts);
-
-export default router;
